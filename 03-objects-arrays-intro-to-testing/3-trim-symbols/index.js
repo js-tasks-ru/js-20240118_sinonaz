ade7b9d753cafd05f21427235d5963fabd3f9aee
Isaac <i@ishir.ru>
2024-01-22 17:48:06 +0300

    moving vars

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,17 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-  let previousLetter;
-  let counter = 0;
 
   if (size === 0) {
     return '';
   } else if (!size) {
     return string;
   } else {
+    let counter = 0;
+    let previousLetter;
+
     return string.split('').map((currentLetter) => {
       counter = currentLetter === previousLetter ? counter += 1 : 1;
       previousLetter = currentLetter;
+
       return counter <= size ? currentLetter : '';
     }).join('');
   }
