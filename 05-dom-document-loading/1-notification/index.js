4fa64ced3941ad2da127f565eb3ea44d5acfadb4
Isaac <i@ishir.ru>
2024-02-07 15:37:56 +0300

    notification component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,55 @@
 export default class NotificationMessage {
+  element;
+  message;
+  duration;
+  type;
+
+  constructor(message, options = {}) {
+    this.message = message ? message : ``;
+    this.type = options.type ? options.type : ``;
+    this.duration = options.duration ? options.duration : 2000;
+
+    if (this.element) {
+      this.destroy();
+    }
+
+    this.element = this.getElement();
+  }
+
+  getElement() {
+    const wrapElement = document.createElement(`div`);
+
+    wrapElement.innerHTML = this.getElementTemplate();
+
+    return wrapElement.firstChild;
+  }
+
+  getElementTemplate() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration}ms">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">${this.message}</div>
+    </div>
+  </div>`;
+  }
+
+  show(parent) {
+    const notificationParentElement = parent ? parent : document.body;
+
+    notificationParentElement.append(this.element);
+
+    setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 
 }
